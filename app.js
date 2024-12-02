411def9ab50f52033a309d57a714f57a9bbbef91
yz2424 <24hs214@s.bgu.ac.jp>
2024-12-02 14:07:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,16 +12,16 @@
             } else if (colour === "白" && number === 0) {
                 result = "中吉";
             } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
                 result = "凶";
+            } else if (colour === "白" && number === 2) {
+                result = "小吉";
+            } else if (colour === "オレンジ" && number === 0) {
+                result = "小吉";
             } else if (colour === "オレンジ" && number === 1) {
                 result = "大吉";
             } else if (colour === "オレンジ" && number === 2) {
                 result = "中吉";
             }
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "今日の運勢は"【" + result + "】です";
         }
