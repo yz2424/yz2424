700c9e75cabb2174607ecc8c37970986c4ab0c7a
yz2424 <24hs214@s.bgu.ac.jp>
2024-12-23 13:52:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -32,32 +32,32 @@
                 result = "末吉";
             } else if (colour === "黒" && number === 1) {
                 result = "半吉";
-            } else if (colour === "紫" && number === 0) {
+            } else if (colour === "紫" && number === 1) {
                 result = "吉";
-            } else if (colour === "赤" && number === 2) {
+            } else if (colour === "赤" && number === 1) {
                 result = "小吉";
             } else if (colour === "オレンジ" && number === 1) {
                 result = "中吉";
             } else if (colour === "青" && number === 0) {
                 result = "大吉";
             }
-                     if (colour === "白" && number === 1) {
+                     if (colour === "白" && number === 0) {
                 result = "凶";
             } else if (colour === "赤" && number === 0) {
                 result = "末凶";
             } else if (colour === "紫" && number === 2) {
                 result = "末小吉";
-            } else if (colour === "オレンジ" && number === 1) {
+            } else if (colour === "オレンジ" && number === 0) {
                 result = "末吉";
-            } else if (colour === "青" && number === 0) {
+            } else if (colour === "青" && number === 2) {
                 result = "半吉";
-            } else if (colour === "ピンク" && number === 2) {
+            } else if (colour === "ピンク" && number === 0) {
                 result = "吉";
-            } else if (colour === "水色" && number === 1) {
+            } else if (colour === "水色" && number === 2) {
                 result = "小吉";
-            } else if (colour === "黒" && number === 0) {
+            } else if (colour === "黒" && number === 2) {
                 result = "中吉";
-            } else if (colour === "黄色" && number === 2) {
+            } else if (colour === "黄色" && number === 1) {
                 result = "大吉";
             }
 
