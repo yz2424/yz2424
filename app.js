a201873e40a0be32c6e0bc53a00af757b0a34307
yz2424 <24hs214@s.bgu.ac.jp>
2024-12-23 14:37:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-        function checkFortune() 
+            function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
