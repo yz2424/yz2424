e5afe4d663036ab3aeac29c841386646543b4ae1
yz2424 <24hs214@s.bgu.ac.jp>
2025-01-20 13:19:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -23,43 +23,7 @@
             } else if (colour === "赤" && number === 2) {
                 result = "大吉";
             
-            } else  if (colour === "黄色" && number === 2) {
-                result = "凶";
-            } else if (colour === "ピンク" && number === 1) {
-                result = "末凶";
-            } else if (colour === "水色" && number === 0) {
-                result = "末小吉";
-            } else if (colour === "白" && number === 2) {
-                result = "末吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "半吉";
-            } else if (colour === "紫" && number === 1) {
-                result = "吉";
-            } else if (colour === "赤" && number === 1) {
-                result = "小吉";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "中吉";
-            } else if (colour === "青" && number === 0) {
-                result = "大吉";
             
-            } else if (colour === "白" && number === 0) {
-                result = "凶";
-            } else if (colour === "赤" && number === 0) {
-                result = "末凶";
-            } else if (colour === "紫" && number === 2) {
-                result = "末小吉";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "末吉";
-            } else if (colour === "青" && number === 2) {
-                result = "半吉";
-            } else if (colour === "ピンク" && number === 0) {
-                result = "吉";
-            } else if (colour === "水色" && number === 2) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "中吉";
-            } else if (colour === "黄色" && number === 1) {
-                result = "大吉";
             
 
             } document.getElementById('result-output').innerText = "今日の運勢は"【" + result + "】です";
